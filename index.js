e3603c80f1ff81288bc5a84eda0eb9f5f2545371
Georgi <1229206@nvna.eu>
2022-02-10 13:35:21 +0200

    za dovurshvane

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-let a = 1
+let a = [1,2,3,4,5] ;
+let b = [6,7,8,9] ;
+
+for (let i = 0; i<a.length; i++);
+for (let j = 0; j<b.length; j++);
+
+const addition = (i,j) => {
+    let x = i + j ;
+    console.log("Subiraneto e =" ,x)
+};
+
+const subtract = (i,j) => {
+    let x = i - j ;
+    console.log("Izvajdaneto e =" ,x)
+};
+
+const multiply = (i,j) => {
+    let x = i * j ;
+    console.log("Umnojenieto e =" ,x)
+};
+
+const division = (i,j) => {
+    let x = i/j;
+    console.log("Deleneto e =",x)
+};
+
+console.log(addition());
+console.log(subtract());
+console.log(multiply());
+console.log(division());
